7a0160412c6c43f3eea6119ab51c27ee7f4141bd
Joe Scotto <contact@joescotto.com>
2022-08-12 18:48:44 -0400

    Extracted code requiring cleanup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ArrayStepper
-A simple SwiftUI component for stepping through the contents of an array. This package is very similar to [TextFieldStepper](https://github.com/joe-scotto/textfieldstepper.git) except it does not include keyboard input and is only for arrays.
+A simple SwiftUI component for stepping through the contents of an array. This is a sister package to [TextFieldStepper](https://github.com/joe-scotto/textfieldstepper.git).
--- Sources/ArrayStepper/ArrayStepper.swift
+++ Sources/ArrayStepper/ArrayStepper.swift
@@ -1,4 +1,126 @@
-public struct ArrayStepper {
+import SwiftUI
+
+public struct ArrayStepper<T: Equatable>: View {
+    @Binding var selected: T
+    @Binding var values: Array<T>
 
     
+    @State private var timer: Timer? = nil
+    @State private var isLongPressing = false
+    
+    private let use: KeyPath<T, String>?
+    private let label: String
+    
+    private var index: Int {
+        values.firstIndex(of: selected) ?? 0
+    }
+
+    public init(selected: Binding<T>, values: Binding<Array<T>>, use: KeyPath<T, String>? = nil, label: String = "") {
+        self._selected = selected
+        self._values = values
+        self.use = use
+        self.label = label
+    }
+
+    public var body: some View {
+        HStack {
+            longPressButton(
+                image: "minus.circle.fill",
+                action: {
+                    if selected != values.first {
+                        selected = values[index - 1]
+                    }
+                },
+                condition: selected == values.first,
+                longAction: .decrement
+            )
+        
+            Spacer()
+            
+            VStack {
+//                Text(aperture.value)
+//                Text("\(selected as! String)")
+                if let use = use {
+                    Text(selected[keyPath: use])
+                } else {
+                    Text(selected as! String)
+                }
+                
+                Text(label)//.textFieldStepperLabel()
+            }
+            
+            Spacer()
+            
+            longPressButton(
+                image: "plus.circle.fill",
+                action: {
+                    if !isLongPressing {
+                        if selected != values.last {
+                            selected = values[index + 1]
+                        }
+                    } else {
+                        invalidateLongPress()
+                    }
+                },
+                condition: selected == values.last,
+                longAction: .increment
+            )
+        }
+    }
+    
+    func longPressButton(image: String, action: @escaping () -> Void, condition: Bool, longAction: Action) -> some View {
+        return Button(action: action, label: {
+            Image(systemName: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 35)
+        })
+//        .textFieldStepperDisabled(condition)
+        .simultaneousGesture(
+            LongPressGesture(minimumDuration: 0.25).onEnded {
+                startTimer($0, action: longAction)
+            }
+        )
+    }
+    
+    enum Action {
+        case decrement,
+             increment
+    }
+    
+    /**
+     * Decreases or increases the doubleValue
+     */
+    func updateSelected(_ action: Action) {
+        switch action {
+            case .decrement :
+                selected = values[index - 1]
+            case .increment :
+                selected = values[index + 1]
+            }
+    }
+    
+    /**
+     * Starts the long press
+     */
+    func startTimer(_ value: LongPressGesture.Value, action: Action) {
+        isLongPressing = true
+        timer = Timer.scheduledTimer(withTimeInterval: 0.25, repeats: true) { _ in
+            // Perform action regardless of actual value
+            updateSelected(action)
+            
+            // If value after action is outside of constraints, stop long press
+            if values[index] == values.first || values[index] == values.last {
+                invalidateLongPress()
+            }
+        }
+    }
+    
+    /**
+     * Stops the long press
+     */
+    func invalidateLongPress() {
+        isLongPressing = false
+        timer?.invalidate()
+    }
 }
