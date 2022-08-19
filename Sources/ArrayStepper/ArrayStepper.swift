143bc88c1fa8a375994bf12271f02009190ba9fc
Joe Scotto <contact@joescotto.com>
2022-08-19 15:16:17 -0400

    Secondary init for sections or values only

--- Sources/ArrayStepper/ArrayStepper.swift
+++ Sources/ArrayStepper/ArrayStepper.swift
@@ -48,9 +48,53 @@ public struct ArrayStepper<T: Hashable>: View {
         self._values = values
         
         // Assign properties
-        self.sections = sections != nil ? sections! : [ArrayStepperSection(header: config.label, items: _values.wrappedValue)]
+        self.sections = sections != nil ? sections! : [ArrayStepperSection(header: config.label, items: _values.wrappedValue)] // Document sections
         self.display = display
         self.config = config
+    }
+    
+    public init(
+        selected: Binding<T>,
+        sections: [ArrayStepperSection<T>],
+        display: @escaping (T) -> String = { "\($0)" },
+        label: String? = nil,
+        incrementSpeed: Double? = nil,
+        decrementImage: ArrayStepperImage? = nil,
+        incrementImage: ArrayStepperImage? = nil,
+        disabledColor: Color? = nil,
+        labelOpacity: Double? = nil,
+        labelColor: Color? = nil,
+        valueColor: Color? = nil,
+        valuesAreUnique: Bool? = nil,
+        selectedCheck: SelectedCheck? = nil,
+        config: ArrayStepperConfig = ArrayStepperConfig()
+    ) {
+        self.init(
+            selected: selected,
+            values: Binding.constant({
+                // Convert sections to values
+                var combinedItems = [T]()
+                
+                for section in sections {
+                    combinedItems += section.items
+                }
+                
+                return combinedItems
+            }()),
+            sections: sections,
+            display: display,
+            label: label,
+            incrementSpeed: incrementSpeed,
+            decrementImage: decrementImage,
+            incrementImage: incrementImage,
+            disabledColor: disabledColor,
+            labelOpacity: labelOpacity,
+            labelColor: labelColor,
+            valueColor: valueColor,
+            valuesAreUnique: valuesAreUnique,
+            selectedCheck: selectedCheck,
+            config: config
+        )
     }
 
     public var body: some View {
@@ -113,11 +157,9 @@ public struct ArrayStepper<T: Hashable>: View {
                 index = selectedIndex
             } else {
                 switch config.selectedCheck {
-                    case .Fail : fatalError("Initial selected value not found for ArrayStepper, please confirm your selected value exists in your values array.")
+                    case .Fail : fatalError("Initial selected value not found for \(config.label) ArrayStepper, please confirm your selected value exists in your values array.")
                     case .First : index = 0
-                    case .Append :
-                        values.append(selected)
-                        index = values.lastIndex
+                    case .Append : values.append(selected); index = values.lastIndex
                 }
             }
         }
